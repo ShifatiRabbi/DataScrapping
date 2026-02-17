97a194e32acf8cf124e0c102b6ec6bfc1c6d7300
ShifatiRabbi <rabbishifati@gmail.com>
2026-02-17 15:11:08 +0600

    fetch data from crickinfo

--- request.js
+++ request.js
@@ -1,22 +1,32 @@
-const request = require("request");
-const cheerio = require("cheerio");
 const chalk = require("chalk");
+const puppeteer = require("puppeteer-extra");
+const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+puppeteer.use(StealthPlugin());
+const url = "https://www.espncricinfo.com/series/icc-men-s-t20-world-cup-2025-26-1502138/sri-lanka-vs-australia-30th-match-group-b-1512748/ball-by-ball-commentary";
 
-const url = "https://tophutbd.com/";
+(async () => {
+    const browser = await puppeteer.launch({
+        headless: true, // important
+        args: ["--no-sandbox", "--disable-setuid-sandbox"]
+     });
+    const page = await browser.newPage();
+    await page.setUserAgent(
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0 Safari/537.36"
+    );
 
-const tagExtractor = (tags) => {};
+    await page.goto(url, { waitUntil: "networkidle2" });
 
-request(url, async (err, res, html) => {
-    if(err) {
-        console.log("Error Occur : " + err);
-    } else {
-        let data = cheerio.load(html);
-        let h3Tags = data("h3");
-        h3Tags.toArray().forEach(h3 => {
-           console.log(chalk.green(data(h3).text())) 
-        });
+    await page.waitForSelector('[itemprop="articleBody"]', { timeout: 20000 });
 
-        let pTags = data("p");
-        console.log(pTags.length);
-    }
-});
+    const commentary = await page.$$eval(
+        '[itemprop="articleBody"]',
+        els => els.map(el => el.innerText)
+    );
+
+    console.log("Latest ball:");
+    console.log(chalk.greenBright(commentary[0]));
+    console.log("*******************************")
+    console.log(chalk.greenBright(commentary[commentary.length - 1]));
+
+    await browser.close();
+})();
